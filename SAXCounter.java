4a2ad5fec9679aff185b84817a90b82a68cdd01d
Oleksiy Oletsky <oletsky@ukr.net>
2020-08-06 15:47:19 +0300

    Add files via upload

--- SAXCounter.java
+++ SAXCounter.java
@@ -6,15 +6,15 @@ import org.xml.sax.helpers.*;
     public class SAXCounter extends DefaultHandler {
         public int cnt;
         String elem;
-        public void startDocument (){
+        public void startDocument(){
             System.out.println("Book catalog: ");
             cnt=0;
         }
-        public void endDocument (){
+        public void endDocument(){
             System.out.println("The amount of  books is  "+cnt);
         }
 
-        public void characters (char [] chars,
+        public void characters(char [] chars,
                                 int startIndex,
                                 int endIndex) {
             String dataString = new String (chars, startIndex, endIndex).trim();
@@ -32,7 +32,7 @@ import org.xml.sax.helpers.*;
             if (elem.equals("author")) System.out.print(cnt+". ");
         }
 
-        public void endElement (String namespaceUri,
+        public void endElement(String namespaceUri,
                                 String localName,
                                 String qualifiedName) {
             if (elem.equals("author")) System.out.print(". ");
